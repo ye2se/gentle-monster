fb45a3afed2d288d3397982e8b5a56b5ce4aa431
ye2se <scscxcvbn@naver.com>
2024-11-18 00:14:42 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,57 @@
+$(function () {
+    const MainVisual = new Swiper('.main_visual_slide', {
+        loop: true,
+        effect: "fade",
+        speed: 1000,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
+    });
+})
+
+$(function () {
+    const mainProductSlide = new Swiper('.main_product_slide', {
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 15,
+        navigation: {
+            nextEl: '.main_product .arrows .next',
+            prevEl: '.main_product .arrows .prev',
+        },
+        breakpoints: {
+            1200: {
+                slidesPerView: 4,
+            },
+            1024: {
+                slidesPerView: 4,
+            },
+            768: {
+                slidesPerView: 3,
+            },
+            500: {
+                slidesPerView: 2,
+            },
+
+        },
+    })
+});
+
+$(function () {
+    $('.header .inner .mopen').on('click', function () {
+        $(this).toggleClass('on');
+        $('.header .inner .gnb').toggleClass('on');
+    });
+})
+
+$(function () {
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+
+        if (sct > 0) {
+            $('.header').addClass('on')
+        } else {
+            $('.header').removeClass('on')
+        }
+    })
+});
